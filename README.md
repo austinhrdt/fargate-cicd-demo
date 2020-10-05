f376f3835562294294b4f1853259d20d92e68535
austinhrdt <austinhrdt@me.com>
2020-10-05 12:08:15 -0400

    upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fargate via GitHub Actions
+
+Using github actions to build and deploy container images to aws ecr. We deploy a single golang microservice api container to our cluster using a task definition.
---
+++ cmd/api/main.go
@@ -0,0 +1,19 @@
+// demo api
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+// home serves home page
+func home(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Hello, World!")
+}
+
+// main application entrypoint
+func main() {
+    http.HandleFunc("/", home)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
