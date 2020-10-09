6845d9cbccd4740918af9ca1aa58394ffac31a99
ahardt013 <austin_hardt@comcast.com>
2020-10-09 14:16:38 -0400

    pre-demo test

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,18 +2,18 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 // home serves home page
-func home(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello, World!")
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, test")
 }
 
 // main application entrypoint
 func main() {
-    http.HandleFunc("/", home)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
